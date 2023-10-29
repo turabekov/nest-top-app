6438b3f354548fe9be558b511a2f72e8ffc4572f
Khumoyun <humoyun.t@payme.uz>
2023-10-29 11:47:51 +0500

    models

--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,1 +1,17 @@
-export class ProductModel {}
+export class ProductModel {
+	image: string;
+	title: string;
+	price: number;
+	oldPrice: number;
+	credit: number;
+	calculatedRating: number;
+	description: string;
+	advantages: string;
+	disadvantages: string;
+	categories: string[];
+	tags: string;
+	characteristics: {
+		[key: string]: string;
+	};
+
+}
