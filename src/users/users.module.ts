103aa5035a5639fade09842281dbbeffd32e9625
ut.turabekov <turabekov.ud@rocketfirm.com>
2023-10-29 18:05:30 +0500

    references in mdb

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './models/user.model';
+import { PostSchema, Post } from './models/post.model';
 
 @Module({
   providers: [UsersService],
   imports: [MongooseModule.forFeature([
-    { name: User.name, schema: UserSchema }
+    { name: User.name, schema: UserSchema },
+    { name: Post.name, schema: PostSchema }
   ])]
 })
 export class UsersModule { }
